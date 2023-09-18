0439af81364a97d1f753f30685c8424d1b2640d6
Iván <ivancelda@outlook.com>
2023-09-18 10:46:09 +0200

    added switch color

--- main.js
+++ main.js
@@ -10,21 +10,32 @@ import './style.scss'
 
 // Llama a esta función para mostrar el mensaje de bienvenida
 displayWelcomeMessage();*/
+  function toggleTheme() {
+    const bodyToggle = document.body;
+    bodyToggle.classList.toggle('dark-theme');
+    //console.log(toggleButton);
+  }
+  document.querySelector('.toggle-theme').addEventListener('click', toggleTheme);
 
-function setCookie() {
-  const welcomeDiv = document.getElementById('welcomeMessage');
 
-  if (!document.cookie.includes('isVisited')) {
-    welcomeDiv.innerHTML = 'BIENVENIDO A MI PÁGINA WEB';
-  }else{
-    welcomeDiv.innerHTML = 'Hola de nuevo';
+document.addEventListener('DOMContentLoaded', function() {
+  function setCookie() {
+    const welcomeDiv = document.getElementById('welcomeMessage');
+
+    if (!document.cookie.includes('isVisited')) {
+      welcomeDiv.innerHTML = 'BIENVENIDO A MI PÁGINA WEB';
+    }else{
+      welcomeDiv.innerHTML = 'Hola de nuevo';
+    }
+
+    const date = new Date();
+    date.setTime(date.getTime() + (365 * 24 * 60 * 60 * 1000));
+    const expires = 'expires=' + date.toUTCString();
+    document.cookie = 'isVisited=true; ' + expires + ';';
   }
 
-  const date = new Date();
-  date.setTime(date.getTime() + (365 * 24 * 60 * 60 * 1000));
-  const expires = 'expires=' + date.toUTCString();
-  document.cookie = 'isVisited=true; ' + expires + ';';
-}
+  // Llama a la función para mostrar el mensaje de bienvenida
+  setCookie();
 
-// Llama a la función para mostrar el mensaje de bienvenida
-setCookie();
+});
+
